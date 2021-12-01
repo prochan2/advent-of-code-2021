bf19319d5763f5275588f04ea0492ec07e82ffa5
Antonín Procházka <antonin@postsharp.net>
2021-12-01 06:12:23 +0100

    day1 task1

--- src/day1/task1/Program.cs
+++ src/day1/task1/Program.cs
@@ -2,4 +2,36 @@
 using System.Linq;
 using static System.Console;
 
-var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
+var lines = File.ReadAllLines(@"..\..\..\input\input.txt");
+
+long? previous = null;
+long current;
+long count = 0;
+
+foreach (var line in lines)
+{
+    current = long.Parse(line);
+
+    try
+    {
+        if (previous == null)
+        {
+            WriteLine($"{line} N/A");
+            continue;
+        }
+
+        if (current > previous)
+        {
+            WriteLine($"{line} +");
+            ++count;
+        }
+
+        WriteLine($"{line} -");
+    }
+    finally
+    {
+        previous = current;
+    }
+}
+
+WriteLine(count);
