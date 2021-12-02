ffe11136b1b07483df9b874535e64e2540cda5f8
Antonín Procházka <antonin@postsharp.net>
2021-12-02 06:24:32 +0100

    day 2 task 2

---
+++ src/day2/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\input\input.txt");
+
+var aim = 0L;
+var h = 0L;
+var v = 0L;
+
+foreach (var line in lines)
+{
+    var instruction = line.Split(' ');
+
+    var offset = long.Parse(instruction[1]);
+
+    switch (instruction[0])
+    {
+        case "forward":
+            h += offset;
+            v += aim * offset;
+            break;
+
+        case "down":
+            aim += offset;
+            break;
+
+        case "up":
+            aim -= offset;
+            break;
+
+        default:
+            throw new InvalidOperationException();
+    }
+}
+
+WriteLine(h * v);
