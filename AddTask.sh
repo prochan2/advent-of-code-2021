7fe8054725b5f8d608a9520a1e3218b695894c6b
Antonín Procházka <antonin@postsharp.net>
2021-12-02 06:13:29 +0100

    day 2 task 1

--- AddTask.sh
+++ AddTask.sh
@@ -1,5 +1,5 @@
 # Usage: AddTask.sh <source day> <source task> <destination day> <new task>
 mkdir -p src/day$3/task$4
-cp src/day$1/task$2/Day$1.Task$2.csproj src/day$3/task$4/Day$3.Task$4.csproj
-cp src/day$1/task$2/Program.cs src/day$3/task$4/Program.cs
+cp -r src/day$1/task$2/* src/day$3/task$4/
+mv src/day$3/task$4/Day$1.Task$2.csproj src/day$3/task$4/Day$3.Task$4.csproj
 dotnet sln add --in-root src/day$3/task$4/Day$3.Task$4.csproj
---
+++ src/day2/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\input\input.txt");
+
+var v = 0L;
+var h = 0L;
+
+foreach (var line in lines)
+{
+    var instruction = line.Split(' ');
+
+    var offset = long.Parse(instruction[1]);
+
+    switch (instruction[0])
+    {
+        case "forward":
+            v += offset;
+            break;
+
+        case "down":
+            h += offset;
+            break;
+
+        case "up":
+            h -= offset;
+            break;
+
+        default:
+            throw new InvalidOperationException();
+    }
+}
+
+WriteLine(v * h);
