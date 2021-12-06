5883383b5741f8618d87ed69cc846d2e6f025a1c
Antonín Procházka <antonin@postsharp.net>
2021-12-06 06:48:21 +0100

    day 6 task 1

---
+++ src/day6/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+var ages = File.ReadAllText(@"..\..\..\input\input.txt").Split(',').Select(n => long.Parse(n)).ToList();
+
+for (int day = 1; day <= 80; day++)
+{
+    int count = ages.Count;
+
+    for (int i = 0; i < count; i++)
+    {
+        if (--ages[i] == -1)
+        {
+            ages[i] = 6;
+            ages.Add(8);
+        }
+    }
+
+    //Write($"Day {day}: ");
+
+    //foreach (var age in ages)
+    //{
+    //    Write($"{age}, ");
+    //}
+
+    //WriteLine();
+}
+
+WriteLine(ages.Count);
